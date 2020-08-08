8d4ff15e684b110a89417ce701d45fe61dbb82a9
Paul Patolla <5120113-BlackRose01@users.noreply.gitlab.com>
2020-08-08 12:21:41 +0200

    implement getter and setter

--- src/main/java/de/blackrose01/model/pulse/Pulse.java
+++ src/main/java/de/blackrose01/model/pulse/Pulse.java
@@ -69,6 +69,150 @@ public class Pulse implements Serializable {
 
     public Pulse() {}
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public int getCategory() {
+        return category;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
+    }
+
+    public boolean isIgnored() {
+        return isIgnored;
+    }
+
+    public void setIgnored(boolean ignored) {
+        isIgnored = ignored;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public long getPulseImage() {
+        return pulseImage;
+    }
+
+    public void setPulseImage(long pulseImage) {
+        this.pulseImage = pulseImage;
+    }
+
+    public PulseImage getPulseImageObject() {
+        return pulseImageObject;
+    }
+
+    public void setPulseImageObject(PulseImage pulseImageObject) {
+        this.pulseImageObject = pulseImageObject;
+    }
+
+    public long getPulseSource() {
+        return pulseSource;
+    }
+
+    public void setPulseSource(long pulseSource) {
+        this.pulseSource = pulseSource;
+    }
+
+    public PulseSource getPulseSourceObject() {
+        return pulseSourceObject;
+    }
+
+    public void setPulseSourceObject(PulseSource pulseSourceObject) {
+        this.pulseSourceObject = pulseSourceObject;
+    }
+
+    public List<Long> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<Long> tags) {
+        this.tags = tags;
+    }
+
+    public long getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(long website) {
+        this.website = website;
+    }
+
+    public PulseUrl getWebsiteObject() {
+        return websiteObject;
+    }
+
+    public void setWebsiteObject(PulseUrl websiteObject) {
+        this.websiteObject = websiteObject;
+    }
+
+    public long getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(long updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public String getChecksum() {
+        return checksum;
+    }
+
+    public void setChecksum(String checksum) {
+        this.checksum = checksum;
+    }
+
     @JsonSetter("pulse_image")
     public void setPulseImageJson(JsonNode jsonNode) {
         if (jsonNode.isInt() || jsonNode.isLong())
